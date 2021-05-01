43cde9bab7716e412d28a16690a17aba9b8195ed
Dipayan Saha <51865387+richik500@users.noreply.github.com>
2021-05-01 13:04:04 +0530

    commit

---
+++ modules/addRecord.py
@@ -0,0 +1,13 @@
+def add_record():
+    print("Enter The Guest Details\n")
+    id = input("Enter Booking ID: ")
+    name = input("Enter Guest Name: ")
+    mob = input("Enter Guest Mobile Number: ")
+    room = input("Enter Guest Room Number: ")
+    ckin = input("Enter Guest Checkin Details: ")
+    ckout = input("Enter Guest Checkout Details: ")
+    rent = input("Enter Rent: ")
+    total = input("Enter Total Bill Amount: ")
+    file = open("Record/customer.txt","a")
+    file.write(id+"-"+name+"-"+mob+"-"+room+"-"+ckin+"-"+ckout+"-"+rent+"-"+total+"\n")
+    file.close()
---
+++ modules/searchBookid.py
@@ -0,0 +1,25 @@
+def search_booking_id():
+    search = input("Enter Booking ID: ")
+    file = open("Record/customer.txt", "r")
+    flag = 0
+    while True:
+        content = file.readline()
+        length = len(content)
+        if length == 0:
+            break
+        arr = content.split("-")
+        if arr[0] == search:
+            print("\n The record has been found ")
+            print("Booking ID: ", arr[0])
+            print("Name: ", arr[1])
+            print("Mob No.: ", arr[2])
+            print("Room No.: ", arr[3])
+            print("Checkin Details: ", arr[4])
+            print("Checkout Details: ", arr[5])
+            print("Rent: ", arr[6])
+            print("Total Amount: ", arr[7])
+            flag = 1
+            break
+    if flag == 0:
+        print("\n Customer Record Not Found \n")
+    file.close()
