3cfc00d2935370baf44e30c2ba4e33cc5ac2e9ff
Dipayan Saha <51865387+richik500@users.noreply.github.com>
2021-05-02 00:47:07 +0530

    commit

---
+++ functions/delCusRec.py
@@ -0,0 +1,24 @@
+import os
+def delete_customer_record():
+    search = input("Enter Booking ID: ")
+    file1 = open("Record/customer.txt", "r")
+    file2 = open("Record/temp.txt", "w")
+    flag = 0
+    while True:
+        content = file1.readline()
+        length = len(content)
+        if length == 0:
+            break
+        arr = content.split("-")
+        if arr[0] != search:
+            file2.write(content)
+        if arr[0] == search:
+            flag = 1
+    file1.close()
+    file2.close()
+    if flag == 1:
+        print("Customer Record Deleted Successfully \n")
+        os.remove("Record/customer.txt")
+        os.rename("Record/temp.txt", "Record/customer.txt")
+    elif flag == 0:
+        print("Deletion Unsuccessful since Customer Record Not Found \n")
